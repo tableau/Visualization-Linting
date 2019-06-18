ec602713b772b600c56cd271f4e366c30d6ca6c3
Andrew McNutt <amcnutt@mac-amcnutt.tsi.lan>
2019-06-18 15:33:47 -0700

    flail around imaginatively, get vega-rendering working

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-export function lint(specification, data) {
-  return [];
+import {parse, View} from 'vega';
+import {compile} from 'vega-lite';
+
+const lintRules = [{
+  name: 'rescaleData',
+  type: 'algebraic-ground',
+  operation: d => d * 1000,
+  evaluator: (oldRendering, newRendering) => {
+
+  }
+}];
+
+export function lint(spec) {
+  return Promise.all(lintRules.map(rule => evaluateLintRule(rule, spec)))
+    .then(results => {
+      return results;
+    });
+}
+
+function evaluateLintRule(rule, spec) {
+  return new Promise((resolve, reject) => {
+    resolve({name: rule.name, passed: true});
+  });
+}
+
+/**
+ * generateVegaRendering, takes in a vega lint spec and returns an svg rendering of it
+ */
+function generateVegaRendering(spec) {
+  return new Promise((resolve, reject) => {
+    const runtime = parse(compile(spec).spec, {renderer: 'none'});
+    const view = new View(runtime, {renderer: 'svg'}).initialize();
+    view
+      .runAsync()
+      .then(() => {
+        view.toSVG(2)
+        .then(x => resolve(x))
+        .catch(e => console.log(e));
+      });
+  });
 }
---
+++ test/root-test.js
@@ -0,0 +1,33 @@
+import tape from 'tape';
+import SeattleWeather from '../seattle-weather.json';
+import {lint} from '../';
+
+const BAR_CHART_SPEC = {
+  data: {
+    values: SeattleWeather
+  },
+  height: 200,
+  width: 200,
+  mark: 'bar',
+  encoding: {
+    x: {
+      timeUnit: 'month',
+      field: 'date',
+      type: 'ordinal'
+    },
+    y: {
+      aggregate: 'mean',
+      field: 'precipitation',
+      type: 'quantitative'
+    }
+  }
+};
+
+tape('initial test', t => {
+  lint(BAR_CHART_SPEC)
+    .then(result => {
+
+      t.deepEqual(result, [], 'should find basic results');
+      t.end();
+    });
+});
