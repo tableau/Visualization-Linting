85e177d3928754a581f9c2a9ec58eeee787a45e8
Andrew McNutt <mcnutt.andrew@gmail.com>
2019-08-29 15:05:52 -0700

    table script updates

--- paper-scripts/prepare-mirage-table.js
+++ paper-scripts/prepare-mirage-table.js
@@ -12,12 +12,12 @@ const toRow = (colorSuffix, name) => (row, idx, rows) => {
   }${mirage}`;
 };
 const CAPTION =
-  'Examples of errors arising at each of the stages in our taxonomy along with the ways that those errors can manifest themselves as mirages. This list does not try to be comprehensive, only evocative.';
+  'Examples of errors arising at each of the stages in our taxonomy along with the ways that those errors can manifest themselves as mirages. A larger table of mirage errors is available in the supplemental materials. Even so, tThis list does not try to be comprehensive, only evocative.';
 const toBlock = (rows, name, colorSuffix) =>
   `${rows.map(toRow(colorSuffix, name)).join('\\\\\n')}\\\\`;
 // % \\multirow{13}{1em}{\\hspace{-0.4cm}\\rotatebox{90}{\\normalsize{\\normalsize{Curation}}}}
 const template = (curating, wrangling, visualizing, comprehending) => `
-\\begin{table*}[]
+\\begin{table*}[h!]
 \\centering
 \\caption{${CAPTION}}
 \\small
@@ -45,7 +45,8 @@ function groupBy(data, key) {
   }, {});
 }
 getFile('./paper-scripts/lint-rules.tsv')
-  .then(d => tsv.parse(d).filter(x => !x.Hide))
+  // .then(d => tsv.parse(d).filter(x => !x.Hide))
+  .then(d => tsv.parse(d).filter(x => x['MC Top 4'] === 'TRUE'))
   .then(d => {
     const groups = groupBy(d, 'Taxonomy (cause)');
     writeFile(
