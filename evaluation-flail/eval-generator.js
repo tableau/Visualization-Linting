5983ddcf4743e6b407735f5178a4d78300fa817c
Andrew McNutt <mcnutt.andrew@gmail.com>
2019-09-06 18:35:00 -0700

    get eval stuff in plkace

--- evaluation-flail/eval-generator.js
+++ evaluation-flail/eval-generator.js
@@ -2,7 +2,7 @@
 import {lint} from '../src';
 import {writeFile, getFile, executePromisesInSeries} from 'hoopoe';
 
-const DEGRADE_SIZE = 1;
+const DEGRADE_SIZE = 5;
 const TRIAL_SIZE = 1;
 const buildChart = (errorType, levelOfDegrade, idx) => {
   return {
@@ -10,7 +10,7 @@ const buildChart = (errorType, levelOfDegrade, idx) => {
     // an internal identifir just for us
     $$$identifier$$$: {errorType, levelOfDegrade, idx},
     data: {
-      url: `./example-data/testData/${errorType}/${levelOfDegrade}/${idx}.csv`
+      url: `./example-data/testData/${errorType}/${levelOfDegrade}/${idx}.csv`,
     },
     mark: 'bar',
     encoding: {
@@ -20,9 +20,9 @@ const buildChart = (errorType, levelOfDegrade, idx) => {
         type: 'quantitative',
         aggregate: 'average',
         scale: {domain: [0, 320]},
-        axis: {title: false}
-      }
-    }
+        axis: {title: false},
+      },
+    },
   };
 };
 
@@ -39,30 +39,32 @@ const allTargets = ['missing', 'outliers', 'repeated'].reduce(
     const content = [...new Array(DEGRADE_SIZE)].reduce(
       (mem, _, levelOfDegrade) => {
         const trials = [...new Array(TRIAL_SIZE)].map((__, idx) =>
-          buildChart(errorType, levelOfDegrade + 10, idx)
+          buildChart(errorType, levelOfDegrade * 2 + 2, idx),
         );
         return mem.concat(trials);
       },
-      []
+      [],
     );
     return acc.concat(content);
   },
-  []
+  [],
 );
 
 console.log('executing evaluations');
 const startTime = new Date().getTime();
+const results = [];
 executePromisesInSeries(
   allTargets.map(chart => () => {
-    console.log(JSON.stringify(chart, null, 2));
+    // console.log(JSON.stringify(chart, null, 2));
     console.log(`STARTING ${chart.data.url}`);
     return lint(chart, {noVisualExplain: true}).then(result => {
       console.log(`FINISHED ${chart.data.url}`);
       console.log(prepareReport(result));
+      results.push({...prepareReport(result)});
       // PROBABLY NEED TO SAVE THIS TO FILE AS WE GO
       return result;
     });
-  })
+  }),
 ).then(() => {
   // console.log(results);
   const endTime = new Date().getTime();
