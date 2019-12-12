470fb34ed9b4e7c982df42d58e9af9a31c6a33bc
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2019-12-11 19:23:20 -0600

    update cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 /* eslint-disable */
 const PORT = process.env.PORT || 5000;
@@ -13,14 +14,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept',
+//   );
+//   next();
+// });
 
 // should cache all of the generated renderings, assign em ids
 app.post('/get-rendering', (req, res) => {
