4d315f40046a8893afe7c364a377ee6c49afad09
mcorrell <michael.a.correll@gmail.com>
2022-10-12 15:24:02 -0700

    Updating packages, prepping for sunset of heroku

--- showcase/webpack.config.js
+++ showcase/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     new MonacoWebpackPlugin({
       // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+      features: ["!gotoSymbol"],
       languages: ['json'],
     }),
   ],
